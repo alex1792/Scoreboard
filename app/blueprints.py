42c0dff20dea7c3d896d2f8908621a5448a125c9
alex1792 <56502742+alex1792@users.noreply.github.com>
2025-04-02 22:38:09 -0700

    Third Commit
    correction on homepage. Adjust the home.html so that it can extend from base.html
    
    Now, we can register, login.

--- app/blueprints.py
+++ app/blueprints.py
@@ -1,5 +1,5 @@
 from flask import Blueprint
 
-home_blueprint = Blueprint('home_blueprint', __name__)
-scoreboard_blueprint = Blueprint('scoreboard_blueprint', __name__)
-umpire_blueprint = Blueprint('umpire_blueprint', __name__)
+home_blueprint = Blueprint('home_blueprint', __name__, template_folder='templates')
+scoreboard_blueprint = Blueprint('scoreboard_blueprint', __name__, template_folder='templates')
+umpire_blueprint = Blueprint('umpire_blueprint', __name__, template_folder='templates')
